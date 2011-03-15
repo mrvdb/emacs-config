004b1ff83f723afe83276ae98f5f34db2ca10107
Marcel van der Boom <marcel@hsdev.com>
2011-03-15 14:12:15 +0100

    Managing color-theme zenburn and removing some cruft

--- local/el-get-settings.el
+++ local/el-get-settings.el
@@ -5,9 +5,15 @@
 ; The reason this is nice that it provides one neat umbrella (in Emacs) to manage packages for Emacs.
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+; FIXME: this has to be here before the color-theme install to prevent a load error
+(add-to-list 'load-path "~/.emacs.d/el-get/color-theme")
+
 (require 'el-get)
 (setq el-get-sources
-      '((:name zenburn)
+      '(
+	(:name color-theme)
+	(:name color-theme-zenburn)
 	(:name smex)
 	(:name rainbow-mode)
 	(:name gnuplot-mode)
