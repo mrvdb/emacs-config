1e751815c1b14cfd46282b944e0858664e2ab565
Marcel van der Boom <marcel@hsdev.com>
2011-03-15 15:28:10 +0100

    Removing tabbar en speedbar for now. It was a half-baked configuration I never finished

--- init.el
+++ init.el
@@ -151,48 +151,6 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
-(require 'tabbar)
- ;; add a buffer modification state indicator in the tab label,
- ;; and place a space around the label to make it looks less crowd
- (defadvice tabbar-buffer-tab-label (after fixup_tab_label_space_and_flag activate)
-   (setq ad-return-value
-		(if (and (buffer-modified-p (tabbar-tab-value tab))
-				 (buffer-file-name (tabbar-tab-value tab)))
-			(concat " + " (concat ad-return-value " "))
-			(concat " " (concat ad-return-value " ")))))
- ;; called each time the modification state of the buffer changed
- (defun ztl-modification-state-change ()
-   (tabbar-set-template tabbar-current-tabset nil)
-   (tabbar-display-update))
- ;; first-change-hook is called BEFORE the change is made
- (defun ztl-on-buffer-modification ()
-   (set-buffer-modified-p t)
-   (ztl-modification-state-change))
- (add-hook 'after-save-hook 'ztl-modification-state-change)
- ;; this doesn't work for revert, I don't know
- ;;(add-hook 'after-revert-hook 'ztl-modification-state-change)
- (add-hook 'first-change-hook 'ztl-on-buffer-modification)
-
-(set-face-attribute
- 'tabbar-default nil
- :background "gray60")
-(set-face-attribute
- 'tabbar-unselected nil
- :background "gray85"
- :foreground "gray30"
- :box nil)
-(set-face-attribute
- 'tabbar-selected nil
- :background "#f2f2f6"
- :foreground "black"
- :box nil)
-(set-face-attribute
- 'tabbar-button nil
- :box '(:line-width 1 :color "gray72" :style released-button))
-(set-face-attribute
- 'tabbar-separator nil
- :height 0.7)
-
 ; erc
 (and
      (require 'erc-highlight-nicknames)
