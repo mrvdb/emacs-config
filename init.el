b4f130ba6f77d38d6ea4fea5ea4fc163b6e68eac
Marcel van der Boom <marcel@hsdev.com>
2011-03-26 12:25:48 +0100

    Split off mail configuration to separated config file

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 
 ; Define where customization should be stored
 ; anything done in custom.el can be overridden in explicit files
+; so we want to load our custom file first  and then the
+; crafted ones.
 (setq custom-file (concat "~/.emacs.d/mrb/custom.el"))
 (load custom-file 'noerror)
 
@@ -19,9 +21,9 @@
 (load "mrb/statusnet")             ; Statusnet configuration
 (load "mrb/google-map")            ; 
 (load "mrb/xmpp")                  ;
+(load "mrb/mail")                  ; Mail confguration
 (load "mrb/openscad")              ; OpenSCAD mode
 (load "mrb/ldap")                  ; LDAP integration
-
 
 ; External packages
 (require 'sudo-save)
@@ -31,32 +33,6 @@
 ;;
 (if window-system  (normal-erase-is-backspace-mode t))
 
-;; Mail and News
-(setq 
- ; User agent style is message mode (gnus, but independent of it)
-  mail-user-agent 'message-user-agent
-
- ; My properties
-  user-full-name "Marcel van der Boom"
-  ; I'd like to use mrb@hsdev.com, but s/mime certificate is bound to
-  ; marcel@hsdev.com although not all cleints are as picky as Mail.app
-  ; for example. Claws thought mail sent with From: set to
-  ; mrb@hsdev.com was OK.  
-  ; On the other hand, Claws was much more picky about					
-  user-mail-address "marcel@hsdev.com"
-
-  ; Sending it
-  smtpmail-default-smtp-server "localhost"
-  smtpmail-local-domain "hsdev.com"
-  smtpmail-sendto-domain "hsdev.com"
-  send-mail-function 'smtpmail-send-it                 ; This is for mail
-  message-send-mail-function 'message-smtpmail-send-it ; This is for gnus
-
-  ; Always put one in the Sent folder on sending
-  message-default-mail-headers "Bcc: mrb+Sent@hsdev.com\n"
-  mail-yank-prefix ">> "
-)
-(require 'smtpmail)
 
 ; S/MIME signing always and automatically
 ; TODO: where can I toggle this on/off while composing?
---
+++ mrb/mail.el
@@ -0,0 +1,28 @@
+;; Mail and News configuration
+(require 'smtpmail)
+
+(setq 
+ ; User agent style is message mode (gnus, but independent of it)
+  mail-user-agent 'message-user-agent
+
+ ; My properties
+  user-full-name "Marcel van der Boom"
+  ; I'd like to use mrb@hsdev.com, but s/mime certificate is bound to
+  ; marcel@hsdev.com although not all cleints are as picky as Mail.app
+  ; for example. Claws thought mail sent with From: set to
+  ; mrb@hsdev.com was OK.  
+  ; On the other hand, Claws was much more picky about					
+  user-mail-address "marcel@hsdev.com"
+
+  ; Sending it
+  smtpmail-default-smtp-server "localhost"
+  smtpmail-local-domain "hsdev.com"
+  smtpmail-sendto-domain "hsdev.com"
+  send-mail-function 'smtpmail-send-it                 ; This is for mail
+  message-send-mail-function 'message-smtpmail-send-it ; This is for gnus
+
+  ; Always put one in the Sent folder on sending
+  message-default-mail-headers "Bcc: mrb+Sent@hsdev.com\n"
+  mail-yank-prefix ">> "
+)
+
