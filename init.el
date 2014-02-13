a5eb4a7127264fc2c6f11350c0f6b11ff4fb08e2
Marcel van der Boom <marcel@hsdev.com>
2014-02-13 14:19:58 +0100

    Path for development sources changed, adjust accordingly

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;; I want to have as little in here as possible. The configuration is
 ;; org-babel based. This means the bootstrap here is to load a proper
 ;; (part of) org-mode and be on our way.
-(add-to-list 'load-path "~/dev/emacs/packages/org-mode/lisp/")
+(add-to-list 'load-path "~/dat/src/emacs/packages/org-mode/lisp/")
 
 ;; Load in the main org file which starts up configuration This will
 ;; lead to an mrb.el file automatically, so that can't exist in the
