fd19996768f71787247a4a7c206bcdbc863c93fe
Marcel van der Boom <marcel@hsdev.com>
2014-12-20 10:10:43 +0100

    Configuring the new default background color

--- custom.el
+++ custom.el
@@ -263,7 +263,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#202020" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "microsoft" :family "Consolas"))))
+ '(default ((t (:inherit nil :stipple nil :background "#300A24" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "microsoft" :family "Consolas"))))
  '(fixed-pitch ((t (:family "Consolas"))))
  '(flyspell-duplicate ((t (:foreground "goldenrod" :underline t :weight bold))))
  '(flyspell-incorrect ((t (:foreground "tomato" :underline t :weight bold))))
