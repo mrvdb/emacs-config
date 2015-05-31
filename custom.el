b3995b0c2154629f918d7e0c647b21d7c5540d20
Marcel van der Boom <marcel@hsdev.com>
2015-05-31 17:18:34 +0200

    Standard height is now 110, not need to specify

--- custom.el
+++ custom.el
@@ -262,7 +262,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#300A24" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#300A24" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(fixed-pitch ((t (:family "Consolas"))))
  '(flyspell-duplicate ((t (:foreground "goldenrod" :underline t :weight bold))))
  '(flyspell-incorrect ((t (:foreground "tomato" :underline t :weight bold))))
