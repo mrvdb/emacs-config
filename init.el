c6165c76c03f6eab432d6d8cbbadcc98dd68d920
Marcel van der Boom <marcel@hsdev.com>
2015-12-22 15:01:11 +0100

    Ok, if package.el wants this, I'll let it have it

--- init.el
+++ init.el
@@ -8,6 +8,13 @@
 
 ;;; Code:
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/dat/src/emacs/packages/org-mode/lisp/")
 
 ;; Load in the main org file which starts up configuration This will
