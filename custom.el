4da3fe0e43fb16aa91b88449ea4d9e71b870d5e4
Marcel van der Boom <marcel@hsdev.com>
2017-02-10 13:31:20 +0100

    Move definition of default face to main config

--- custom.el
+++ custom.el
@@ -282,7 +282,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray14" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(company-scrollbar-bg ((((class color) (min-colors 89)) (:background "#151515"))))
  '(company-tooltip ((((class color) (min-colors 89)) (:foreground "#ffffff" :background "#151515"))))
  '(company-tooltip-common ((((class color) (min-colors 89)) (:foreground "#cf6a4c" :background "#151515"))))
