43fa9cc13fd7293bd90a7fd5129fe6da4b84417d
Marcel van der Boom <marcel@hsdev.com>
2017-07-12 11:56:01 +0200

    Simplify startup

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;(package-initialize)
+(package-initialize)
 
 (add-to-list 'load-path "~/dat/src/emacs/packages/org-mode/lisp/")
 
