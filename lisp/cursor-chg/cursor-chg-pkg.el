b5f3556a5eaffed15612cdbf555fda1cb03d135f
Marcel van der Boom <marcel@hsdev.com>
2019-11-21 13:31:21 +0100

    Remove unused cursor-chg package

--- lisp/cursor-chg/cursor-chg-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cursor-chg" "20170221.1610" "Change cursor dynamically, depending on the context." 'nil :url "https://www.emacswiki.org/emacs/download/cursor-chg.el" :keywords '("cursor" "accessibility"))
