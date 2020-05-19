0a705bd8ea6f8931c4889ddd5d216b12e71661a7
Marcel van der Boom <marcel@hsdev.com>
2020-05-19 16:03:12 +0200

    We don't need a separate thing for fixed-pitched face, right?
    Everything we do in Emacs is fixed-pitch

--- custom.el
+++ custom.el
@@ -210,5 +210,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :slant normal :weight normal :height 113 :width normal))))
- '(fixed-pitch ((t (:family "DejaVu Sans Mono")))))
+ '(default ((t (:family "Hack" :slant normal :weight normal :height 113 :width normal)))))
