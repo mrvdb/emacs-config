4f4639b7e7e458df826b48484c296be577623137
Marcel van der Boom <marcel@hsdev.com>
2022-03-28 08:36:17 +0200

    Upgrade mu

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -73,7 +73,7 @@
  ("melpa" . "4b695ee03d16e9253674cedaef785c2545113b67")
  ("message-view-patch" . "40bc2e554fc1d0b6f0c403192c0a3ceaa019a78d")
  ("mic-paren" . "d0410c7d805c9aaf51a1bcefaaef092bed5824c4")
- ("mu" . "056fecd6aa7a0b231b28b62c46bde4a535baf6d2")
+ ("mu" . "5a28cdfba4743d9d771fc5ee432ce23ca834b38a")
  ("mu4e-alert" . "91f0657c5b245a9de57aa38391221fb5d141d9bd")
  ("mu4e-jump-to-list.el" . "4d362a668be4ae624ee96bf7806b25505b4bdf5c")
  ("mu4e-query-fragments.el" . "8d93ede3772353e2dbc307de03e06e37ea6a0b6c")
