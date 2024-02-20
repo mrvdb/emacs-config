8d31a07c73cd68499b9c832cac5f65daf9f4e3cb
Marcel van der Boom <marcel@hsdev.com>
2024-02-20 11:50:27 +0100

    Some early init settings I pickup up somewhere

--- early-init.el
+++ early-init.el
@@ -1,2 +1,7 @@
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
+
+;; Dont popup a warnings buffer for native-comp errors
+(setq native-comp-async-report-warnings-errors 'silent)
+
+(provide 'early-init)
