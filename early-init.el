44854a1cdc2974784269bdeb7bdef25b4ca57463
Marcel van der Boom <marcel@hsdev.com>
2024-05-08 13:38:46 +0200

    Some early init additions which made sense to me

--- early-init.el
+++ early-init.el
@@ -1,5 +1,7 @@
-;; Disable package.el in favor of straight.el
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil     ; Disable package.el in favor of straight.el
+      inhibit-startup-message t
+      frame-resize-pixelwise t
+      package-native-compile t)
 
 ;; Dont popup a warnings buffer for native-comp errors
 (setq native-comp-async-report-warnings-errors 'silent)
