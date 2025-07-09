67a9c1539c251991a60211fdd06a531011cf361e
Marcel van der Boom <marcel@hsdev.com>
2025-07-09 18:12:56 +0200

    Changed some fonts, storing in custom.el for now until I get it to config file

--- custom.el
+++ custom.el
@@ -102,4 +102,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :slant normal :weight normal :height 112 :width normal)))))
+ '(default ((t (:family "Hack" :slant normal :weight normal :height 112 :width normal))))
+ '(font-lock-comment-face ((t (:foreground "bisque4"))))
+ '(org-headline-done ((t (:inherit font-lock-comment-face)))))
